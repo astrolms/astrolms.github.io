788d628d8b4e241842d65baad9195e94e4c8fb95
astrolms <156137368+astrolms@users.noreply.github.com>
2024-01-09 23:16:24 +0100

    Update media.js

--- media.js
+++ media.js
@@ -27,7 +27,7 @@ $(document).ready(function() {
          }, function() {
         // on mouseout, reset the background colour
          $('#1s').css('color', '#333');
-		 $('#1').attr('src','photo/gemini.png');
+		 $('#1').attr('src','gemini.png');
 		 $('#1st').css('color', 'white');
          });
          });
@@ -37,11 +37,11 @@ $(document).ready(function() {
          $('#2nd').hover(function() {
          $('#2s').css('color', 'white');
 		 $('#2nd').css('color', '#030303');
-		 $('#2').attr('src','photo/leo2.png');
+		 $('#2').attr('src','leo2.png');
          }, function() {
         // on mouseout, reset the background colour
          $('#2s').css('color', '#333');
-		 $('#2').attr('src','photo/leo.png');
+		 $('#2').attr('src','leo.png');
 		 $('#2nd').css('color', 'white');
          });
          });
@@ -51,11 +51,11 @@ $(document).ready(function() {
          $('#3rd').hover(function() {
          $('#3s').css('color', 'white');
 		 $('#3rd').css('color', '#030303');
-		 $('#3').attr('src','photo/taurus2.png');
+		 $('#3').attr('src','taurus2.png');
          }, function() {
         // on mouseout, reset the background colour
          $('#3s').css('color', '#333');
-		 $('#3').attr('src','photo/taurus.png');
+		 $('#3').attr('src','taurus.png');
 		 $('#3rd').css('color', 'white');
          });
          });
@@ -65,11 +65,11 @@ $(document).ready(function() {
          $('#4th').hover(function() {
          $('#4s').css('color', 'white');
 		 $('#4th').css('color', '#030303');
-		 $('#4').attr('src','photo/aries2.png');
+		 $('#4').attr('src','aries2.png');
          }, function() {
         // on mouseout, reset the background colour
          $('#4s').css('color', '#333');
-		 $('#4').attr('src','photo/aries.png');
+		 $('#4').attr('src','aries.png');
 		 $('#4th').css('color', 'white');
          });
          });
@@ -84,11 +84,11 @@ $(document).ready(function() {
          $('#5th').hover(function() {
          $('#5s').css('color', 'white');
 		 $('#5th').css('color', '#030303');
-		 $('#5').attr('src','photo/cancer2.png');
+		 $('#5').attr('src','cancer2.png');
          }, function() {
         // on mouseout, reset the background colour
          $('#5s').css('color', '#333');
-		 $('#5').attr('src','photo/cancer.png');
+		 $('#5').attr('src','cancer.png');
 		 $('#5th').css('color', 'white');
          });
          });
