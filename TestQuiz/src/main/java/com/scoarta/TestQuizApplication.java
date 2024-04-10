f715c7eecdd01a5af68254de9bcb4b8ce4caf3c3
astrolms <156137368+astrolms@users.noreply.github.com>
2024-04-10 11:54:00 +0200

    Add files via upload

---
+++ TestQuiz/src/main/java/com/scoarta/TestQuizApplication.java
@@ -0,0 +1,13 @@
+package com.scoarta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestQuizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestQuizApplication.class, args);
+	}
+
+}
---
+++ TestQuiz/src/test/java/com/scoarta/TestQuizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scoarta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestQuizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
